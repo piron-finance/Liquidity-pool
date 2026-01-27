1bd4a84f482b97a17686c8ebd9f6c085545bdbc6
Ernest Nnamdi <ernestnamdi66@gmail.com>
2026-01-27 03:23:54 +0100

    temp readme

--- src/libraries/ValidationLibrary.sol
+++ src/libraries/ValidationLibrary.sol
@@ -38,7 +38,6 @@ library ValidationLibrary {
         require(receiver != address(0), "ValidationLibrary/invalid receiver");
         require(poolRegistry.isRegisteredPool(poolAddress), "ValidationLibrary/invalid pool");
         
-        // Check minimum investment
         require(assets >= poolData.config.minInvestment, "ValidationLibrary/below minimum investment");
 
         require(poolData.status == IPoolTypes.PoolStatus.FUNDING, "ValidationLibrary/not funding phase");
